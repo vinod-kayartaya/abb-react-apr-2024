50143a873c8ae4cd7d8c8f5405369fc8956d710f
Vinod Kumar Kayartaya <vinod@vinod.co>
2024-04-11 17:25:50 +0530

    day 2 end

--- examples/react-redux-customer-dashboard/src/components/CustomerDetails.js
+++ examples/react-redux-customer-dashboard/src/components/CustomerDetails.js
@@ -7,6 +7,7 @@ import {
 
 const CustomerDetails = () => {
   const { selectedCustomer } = useSelector((store) => store.customersReducer);
+
   const dispatch = useDispatch();
 
   const deleteButtonHandler = () => {
