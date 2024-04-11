910c3b80f1a9777079d2cb60063d2db57bc8acbd
Anurag Gupta <15166401+theanuraggupta@users.noreply.github.com>
2024-04-11 14:59:18 +0530

    delete confirmation of customers using dispatch

--- examples/react-redux-customer-dashboard/src/components/CustomerCard.js
+++ examples/react-redux-customer-dashboard/src/components/CustomerCard.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteCustomer } from "../redux/actions/customers-action-creators";
 
 const CustomerCard = ({ customer }) => {
+  const dispatch = useDispatch();
+
+  const deleteButtonHandler = () => {
+    if (!window.confirm("Are you sure you want to delete?")) return;
+
+    dispatch(deleteCustomer(customer.id));
+  };
   return (
     <>
-      <div className="row">
+      <div className="row app-customer-card">
         <div className="col-4">
           <img
             src={customer.picture}
@@ -13,9 +22,12 @@ const CustomerCard = ({ customer }) => {
         </div>
         <div className="col-8">
           <h3>{customer.name}</h3>
-          <div>
+          <div className="app-customer-card-buttons">
             <button className="btn btn-link bi bi-search"></button>
-            <button className="btn btn-link bi bi-trash"></button>
+            <button
+              className="btn btn-link bi bi-trash"
+              onClick={deleteButtonHandler}
+            ></button>
             <button className="btn btn-link bi bi-pen"></button>
           </div>
         </div>
--- examples/react-redux-customer-dashboard/src/index.js
+++ examples/react-redux-customer-dashboard/src/index.js
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
